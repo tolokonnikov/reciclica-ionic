8fb3698d1a2be42010fa2763906728ed2c003f4f
Albert Tolokonnikov <tolokonnikov.albert@gmail.com>
2023-01-12 12:57:36 +0200

    add pickup-call-card shared component

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+import { PickupCallCardComponent } from './pickup-call-card/pickup-call-card.component';
+
+// In this example, MyComponent would be imported as follows:
+// import { MyComponent } from './my-component/my-component.component';
+
+@NgModule({
+  declarations: [
+    PickupCallCardComponent,
+    /*Declare your components here. e.g. MyComponent*/
+  ],
+  providers: [],
+  imports: [
+    CommonModule,
+    IonicModule,
+    // Add modules to import here. e.g. FormsModule
+  ],
+  exports: [
+    PickupCallCardComponent,
+    /*Same as declarations*/
+  ],
+})
+export class ComponentsModule {}
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,19 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { IonicModule } from '@ionic/angular';
-
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+import { IonicModule } from '@ionic/angular';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
+  declarations: [HomePage],
   imports: [
     CommonModule,
     FormsModule,
+    HomePageRoutingModule,
     IonicModule,
-    HomePageRoutingModule
+    ComponentsModule,
   ],
-  declarations: [HomePage]
 })
 export class HomePageModule {}
