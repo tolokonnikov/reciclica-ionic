30e90dda2392308cb2f20afbf251c38ac4c59688
DESKTOP-VVVLAIA\tolok <tolokonnikov.albert@gmail.com>
2023-01-13 16:28:06 +0200

    add tdd navigation

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -1,19 +1,24 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { LoginPage } from './login.page';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), AppRoutingModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginPage);
+    router = TestBed.get(Router);
+
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
@@ -21,4 +26,19 @@ describe('LoginPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should go to home after login', () => {
+    spyOn(router,'navigate');
+    component.login();
+
+    expect(router.navigate).toHaveBeenCalledOnceWith(['home']);
+  });
+
+  it('should go to home after register', () => {
+    spyOn(router,'navigate');
+    component.register();
+
+    expect(router.navigate).toHaveBeenCalledOnceWith(['register']);
+  });
+
 });
