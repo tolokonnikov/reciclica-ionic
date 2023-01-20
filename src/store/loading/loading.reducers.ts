dd6889b2bf6352231ba5b0615075a0d18c47f13e
Albert Tolokonnikov <tolokonnikov.albert@gmail.com>
2023-01-21 00:55:02 +0200

    changes

--- src/store/loading/loading.reducers.ts
+++ src/store/loading/loading.reducers.ts
@@ -2,20 +2,12 @@ import { createReducer, on } from '@ngrx/store';
 import { hide, show } from './loading.actions';
 import { LoadingStste } from './LoadingState';
 
-<<<<<<< HEAD
 const inititalState: LoadingStste = {
-=======
-const initialState: LoadingStste = {
->>>>>>> 55de50a71454c7707daed44d77aad0700d559133
   show: false,
 };
 
 const reducer = createReducer(
-<<<<<<< HEAD
   inititalState,
-=======
-  initialState,
->>>>>>> 55de50a71454c7707daed44d77aad0700d559133
   on(show, () => {
     return { show: true };
   }),
@@ -24,6 +16,6 @@ const reducer = createReducer(
   })
 );
 
-export function loadingReducer(state : LoadingStste, action) {
+export function loadingReducer(state: LoadingStste, action) {
   return reducer(state, action);
 }
