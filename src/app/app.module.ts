ff2784d66cce5cae80e15eef06d7cf1af313261f
Albert Tolokonnikov <tolokonnikov.albert@gmail.com>
2023-01-21 00:59:02 +0200

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,23 +8,22 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppStoreModule } from 'src/store/AppStoreModule';
-import { StoreDevtools } from '@ngrx/store-devtools';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools/src';
-import { ComponentsModule } from "./components/components.module";
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
-    declarations: [AppComponent],
-    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule,
-        ReactiveFormsModule,
-        IonicModule.forRoot(),
-        ...AppStoreModule,
-        StoreDevtoolsModule.instrument({ maxAge: 25 }),
-        ComponentsModule
-    ]
+  declarations: [AppComponent],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IonicModule.forRoot(),
+    ...AppStoreModule,
+    StoreDevtoolsModule.instrument({ maxAge: 25 }),
+    ComponentsModule,
+  ],
 })
 export class AppModule {}
